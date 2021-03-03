b64851e8b46aaa994d4a4781e559e6d65ffa626f
李承斌 <15031617367@163.com>
2021-03-03 14:15:36 +0800

    兼容版bind实现

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-function bind(asThis,...args){
-    // this is function 
-    const fn = this;
-    return function(...args2){
-        return fn.call(asThis,...args,...args2)
+var slice = Array.prototype.slice;
+function bind(asThis){
+    // this is function
+    var args = slice.call(arguments,1)
+    var fn = this;
+    if(typeof fn !== "function"){
+        throw new Error('bind 必须调用在函数身上')
+    }
+    return function(){
+        var args2 = slice.call(arguments,0)
+        return fn.apply(asThis,args.concat(args2));
     }
 }
 module.exports =  bind;
